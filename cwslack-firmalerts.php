3b9346251800364a3a16461d016a2d1a1fd6936f
jundis <jundis@gmail.com>
2016-10-06 18:10:29 -0500

    Completed alert system for #39

--- cwslack-firmalerts.php
+++ cwslack-firmalerts.php
@@ -26,10 +26,12 @@ $apicompanyname = strtolower($companyname); //Company name all lower case for ap
 $authorization = base64_encode($apicompanyname . "+" . $apipublickey . ":" . $apiprivatekey); //Encode the API, needed for authorization.
 
 //Dates required for URL to function
-$datenow = gmdate("Y-m-d\TH:i");
+$datenow = gmdate("Y-m-d\TH:i", strtotime("-10 minutes"));
 $date2hours = gmdate("Y-m-d\TH:i", strtotime("+2 hours"));
 
 $url = $connectwise. "/v4_6_release/apis/3.0/schedule/entries?conditions=status/Name=%27Firm%27%20and%20dateStart%20%3E%20[" . $datenow . "]%20and%20dateStart%20%3C%20[". $date2hours . "]&orderBy=dateStart%20desc";
+$ticketurl = $connectwise . "/v4_6_release/services/system_io/Service/fv_sr100_request.rails?service_recid="; //Set the URL required for ticket links.
+
 
 //Set headers for cURL requests. $header_data covers API authentication while $header_data2 covers the Slack output.
 $header_data =array(
@@ -64,7 +66,142 @@ curl_close($ch);
 
 $dataTData = json_decode($curlBodyTData); //Decode the JSON returned by the CW API.
 
-//REMOVE LATER var_dump($dataTData);
+var_dump($dataTData);
 
+foreach($dataTData as $entry)
+{
+	$reminder = $entry->reminder->name;
+	$user = $entry->member->identifier;
+	$username = $entry->member->name;
+	$namearray = explode(" - ",$entry->name);
+	var_dump($namearray);
+	$companyarray = explode(" / ",$namearray[0]);
+	$company = $companyarray[0];
+	$summary = $namearray[1];
+	$datenow = date("Y-m-d\TH:i"); //Reusing datenow as non-GMT based time.
+	$datestart = date("Y-m-d\TH:i",strtotime($entry->dateStart));
+	
+	if($reminder != "0 minutes" && $posttousers == 1)
+	{
+		$datereminder = date("Y-m-d\TH:i",strtotime($entry->dateStart . " -" . $reminder));
+		
+		if($datenow==$datereminder)
+		{
+			$postfieldspre = array(
+				"channel"=>"@".$user,
+				"attachments"=>array(array(
+					"fallback" => "Firm with " . $company . " in " . $reminder . ".",
+					"title" => "<" . $ticketurl . $entry->objectId . "&companyName=" . $companyname . "|#" . $entry->objectId . ">: " . $summary,
+					"pretext" => "Firm starting in " . $reminder,
+					"text" =>  "You have a firm ticket with " . $company . " coming up. Please wrap up work on current ticket.",
+					"mrkdwn_in" => array(
+						"text",
+						"pretext"
+						)
+					))
+				);
+			$ch = curl_init();
+			$postfields = json_encode($postfieldspre);
 
+			$curlOpts = array(
+				CURLOPT_URL => $webhookurl,
+				CURLOPT_RETURNTRANSFER => true,
+				CURLOPT_HTTPHEADER => $header_data2,
+				CURLOPT_FOLLOWLOCATION => true,
+				CURLOPT_POSTFIELDS => $postfields,
+				CURLOPT_POST => 1,
+				CURLOPT_HEADER => 1,
+			);
+			curl_setopt_array($ch, $curlOpts);
+			$answer = curl_exec($ch);
+
+			// If there was an error, show it
+			if (curl_error($ch)) {
+				die(curl_error($ch));
+			}
+			curl_close($ch);
+		}
+	}
+	
+	if($datenow == $datestart)
+	{
+		if($posttousers==1)
+		{
+			$postfieldspre = array(
+				"channel"=>"@".$user,
+				"attachments"=>array(array(
+					"fallback" => "Firm with " . $company . " starting now.",
+					"title" => "<" . $ticketurl . $entry->objectId . "&companyName=" . $companyname . "|#" . $entry->objectId . ">: " . $summary,
+					"pretext" => "Firm starting now.",
+					"text" =>  "You have a firm ticket with " . $company . " starting now. You should be calling the client now.",
+					"mrkdwn_in" => array(
+						"text",
+						"pretext"
+						)
+					))
+				);
+			$ch = curl_init();
+			$postfields = json_encode($postfieldspre);
+
+			$curlOpts = array(
+				CURLOPT_URL => $webhookurl,
+				CURLOPT_RETURNTRANSFER => true,
+				CURLOPT_HTTPHEADER => $header_data2,
+				CURLOPT_FOLLOWLOCATION => true,
+				CURLOPT_POSTFIELDS => $postfields,
+				CURLOPT_POST => 1,
+				CURLOPT_HEADER => 1,
+			);
+			curl_setopt_array($ch, $curlOpts);
+			$answer = curl_exec($ch);
+
+			// If there was an error, show it
+			if (curl_error($ch)) {
+				die(curl_error($ch));
+			}
+			curl_close($ch);
+		}
+		if($posttochan==1)
+		{
+			$postfieldspre = array(
+				"channel"=>$timechan,
+				"attachments"=>array(array(
+					"fallback" => "Firm for " . $username . " with " . $company . " now.",
+					"title" => "<" . $ticketurl . $entry->objectId . "&companyName=" . $companyname . "|#" . $entry->objectId . ">: " . $summary,
+					"pretext" => $username . " has a firm starting now.",
+					"text" =>  "Please remind the technician of this appointment with " . $company . ". They should be on the phone with them or calling them shortly.",
+					"mrkdwn_in" => array(
+						"text",
+						"pretext"
+						)
+					))
+				);
+			$ch = curl_init();
+			$postfields = json_encode($postfieldspre);
+
+			$curlOpts = array(
+				CURLOPT_URL => $webhookurl,
+				CURLOPT_RETURNTRANSFER => true,
+				CURLOPT_HTTPHEADER => $header_data2,
+				CURLOPT_FOLLOWLOCATION => true,
+				CURLOPT_POSTFIELDS => $postfields,
+				CURLOPT_POST => 1,
+				CURLOPT_HEADER => 1,
+			);
+			curl_setopt_array($ch, $curlOpts);
+			$answer = curl_exec($ch);
+
+			// If there was an error, show it
+			if (curl_error($ch)) {
+				die(curl_error($ch));
+			}
+			curl_close($ch);
+		}
+	}
+	
+	echo $datereminder . "\n\n";
+	echo $datenow . "\n\n";
+	echo $datestart . "\n\n";
+	var_dump($entry);
+}
 ?>
