eabed595b72985a27036aed131f1f007f15244b2
jundis <jundis@gmail.com>
2016-10-21 11:21:38 -0500

    Cleanup unused variables.

--- cwslack-activities.php
+++ cwslack-activities.php
@@ -32,24 +32,17 @@ $exploded = explode("|",$_GET['text']); //Explode the string attached to the sla
 
 //Check to see if the first command in the text array is actually help, if so redirect to help webpage detailing slash command use.
 if ($exploded[0]=="help") {
-    $test=json_encode(array("parse" => "full", "response_type" => "in_channel","text" => "Please visit " . $helpurl . " for more help information","mrkdwn"=>true)); //Encode a JSON response with a help URL.
-    echo $test; //Return the JSON
-    return; //Kill the connection.
+    die(json_encode(array("parse" => "full", "response_type" => "in_channel","text" => "Please visit " . $helpurl . " for more help information","mrkdwn"=>true))); //Encode a JSON response with a help URL.
 }
 
 $urlactivities = $connectwise . "/v4_6_release/apis/3.0/sales/activities/";
-$activityurl = $connectwise . '/v4_6_release/ConnectWise.aspx?fullscreen=false&locale=en_US#startscreen=activity_detail&state={"p":"activity_detail", "s":{"p":{"pid":3, "rd": ';
+$activityurl = $connectwise . '/v4_6_release/ConnectWise.aspx?fullscreen=false&locale=en_US#startscreen=activity_detail&state={"p":"activity_detail", "s":{"p":{"pid":3, "rd":';
 $activityurl2 = ' ,"compId":0, "contId":0, "oppid":0}}}';
 
-$utc = time(); //Get the time.
-// Authorization array. Auto encodes API key for auhtorization above.
-$header_data =array(
- "Authorization: Basic ". $authorization,
-);
 // Authorization array, with extra json content-type used in patch commands to change tickets.
-$header_data2 =array(
-"Authorization: Basic " . $authorization,
- "Content-Type: application/json"
+$header_data =array(
+    "Authorization: Basic " . $authorization,
+    "Content-Type: application/json"
 );
 
 $command=NULL; //Create a command variable and set it to Null
@@ -64,7 +57,7 @@ $dataResponse = array();
 if($command=="new") {
     $dataResponse = cURLPost(
         $urlactivities,
-        $header_data2,
+        $header_data,
         "POST",
         array("name"=>$exploded[1],"status"=>array("id"=>1),"assignTo"=>array("identifier"=>$exploded[2])));
 }
