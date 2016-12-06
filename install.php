d55cd11991db4ba071e8108e95000549bb65c285
jundis <jundis@gmail.com>
2016-12-06 15:28:46 -0600

    Update license statement

--- install.php
+++ install.php
@@ -1,4 +1,24 @@
-<?php ini_set('display_errors', 1); //Display errors in case something occurs ?>
+<?php
+/*
+	CWSlack-SlashCommands
+    Copyright (C) 2016  jundis
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+ini_set('display_errors', 1); //Display errors in case something occurs
+?>
 <html lang="en">
     <head>
         <meta charset="utf-8">
